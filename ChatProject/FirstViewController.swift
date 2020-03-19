ae25d05c338bf7577da77023f4e4707093955a02
hatk <ha.trankhanh@hoozing.onmicrosoft.com>
2020-03-19 23:29:50 +0700

    Initial Commit

---
+++ ChatProject/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ChatProject
+//
+//  Created by Trần Khánh Hà on 3/19/20.
+//  Copyright © 2020 Trần Khánh Hà. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
